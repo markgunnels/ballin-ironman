146e4c699726318934aff41f8358885526e92d68
Mark Gunnels <mark@mark-Lemur-Ultra.(none)>
2012-07-16 09:20:46 -0400

    lein project setup

---
+++ src/ballin_ironman/core.clj
@@ -0,0 +1,6 @@
+(ns ballin-ironman.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/ballin_ironman/core_test.clj
@@ -0,0 +1,7 @@
+(ns ballin-ironman.core-test
+  (:use clojure.test
+        ballin-ironman.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
